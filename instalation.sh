abb3bac476b580f51708a555e1366da16ba6c48d
GLozada99 <20170511@ce.pucmm.edu.do>
2021-04-14 10:08:55 -0400

    Se añadió info al Readme

--- instalation.sh
+++ instalation.sh
@@ -5,7 +5,13 @@ sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 1
 pip install -Iv mariadb==1.0.6
 pip install -Iv beautifultable==1.0.1
 pip install -Iv RPi==0.0.1
-pip install -Iv pyconcrete==0.12.1
+git clone https://github.com/Falldog/pyconcrete
+cd pyconcrete
+echo "T3khnetos123\nT3khnetos123" | sudo python setup.py install
+sudo cp /usr/local/bin/pyconcrete /usr/bin/
+cd ..
+rm pyconcrete
+#pip install -Iv pyconcrete==0.12.1
 
 echo $"export PATH=\$PATH:$(pwd)/Bash" >> ~/.bash_profile
 
